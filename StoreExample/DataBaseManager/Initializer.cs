423863cd267b6acb286147725f0a3557f9c6d24f
remziasanov <35073857+remziasanov@users.noreply.github.com>
2018-03-22 22:16:27 +0300

    Added Initializer

---
+++ StoreExample/DataBaseManager/Initializer.cs
@@ -0,0 +1,33 @@
+﻿using StoreExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StoreExample.DataBaseManager
+{
+    public class Initializer : DropCreateDatabaseAlways<DataBaseContext>
+    {
+        protected override void Seed(DataBaseContext context)
+        {
+            base.Seed(context);
+            context.Roles.Add(new Role { Id = 1, Name = "Admin" });
+            context.Roles.Add(new Role { Id = 2, Name = "Purchase" });
+            context.Purchases.Add(new Purchase
+            {
+                Id = Guid.NewGuid(),
+                Email = "remziasanov@gmail.com",
+                Name = "Admin",
+                SurName = "Admin",
+                Password = "Admin123456789",
+                Phone = "+79788998955",
+                Address = "Simferopol, Gorkogo 11",
+                IndexPost = "290000",
+                RoleId = 1
+            });
+            context.Phones.Add(new PhoneItem { Id = Guid.NewGuid(), ModelOfPhone = "Iphone X", Description = "Крутой телефон", Category = "Смартфоны", SubCategory = "IOS", Price = 75000, ImgUrl = "" });
+            base.Seed(context);
+        }
+    }
+}
