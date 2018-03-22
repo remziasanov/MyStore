efcb789e3cd49413b29d5a171c2cd7979da10382
remziasanov <35073857+remziasanov@users.noreply.github.com>
2018-03-22 11:17:49 +0300

    Models for Registration

---
+++ StoreExample/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StoreExample.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Некорректный email")]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        [Required]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+    }
+}
---
+++ StoreExample/Models/PhoneItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StoreExample.Models
+{
+    public class PhoneItem
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "Модель")]
+        public string ModelOfPhone { get; set; }
+        [Display(Name = "Компания изготовитель")]
+        public string Company { get; set; }
+        [Display(Name = "Описание")]
+        public string Description { get; set; }
+        [Display(Name = "Цена")]
+        public int Price { get; set; }
+        [DataType(DataType.ImageUrl)]
+        [Display(Name = "Изображение")]
+        public string ImgUrl { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Длина строки должна быть от 2 до 50 символов")]
+        [Display(Name = "Категория")]
+        public string Category { get; set; }
+        [Display(Name = "Подкатегория")]
+        public string SubCategory { get; set; }
+    }
+}
