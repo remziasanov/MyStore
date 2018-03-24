54d914ed216114d891048ba5fd53b0bb05fe5156
remziasanov <35073857+remziasanov@users.noreply.github.com>
2018-03-24 14:23:58 +0300

    Added paging helpers

---
+++ StoreExample/Models/PageInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreExample.Models
+{
+    public class PageInfo
+    {
+        public int PageNumber { get; set; } //номер страницы
+        public int PageSize { get; set; } //количество товаров на странице
+        public int TotalItems { get; set; } //всего товаров
+        public int TotalPages //всего страниц
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems / PageSize); }
+        }
+    }
+    public class IndexViewModel
+    {
+        public IEnumerable<PhoneItem> Instruments { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+}
---
+++ StoreExample/MyHtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,35 @@
+﻿using StoreExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreExample.MyHtmlHelpers
+{
+    public static class PagingHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html,
+      PageInfo pageInfo, Func<int, string> pageUrl)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 1; i <= pageInfo.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageUrl(i));
+                tag.InnerHtml = i.ToString();
+                // если текущая страница, то выделяем ее,
+                // например, добавляя класс
+                if (i == pageInfo.PageNumber)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn btn-default");
+                result.Append(tag.ToString());
+            }
+            return MvcHtmlString.Create(result.ToString());
+        }
+    }
+}
