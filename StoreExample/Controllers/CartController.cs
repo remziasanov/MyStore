8b4856a3b7243ba37beea0106094ef58724983a4
remziasanov <35073857+remziasanov@users.noreply.github.com>
2018-04-04 20:52:50 +0300

    edited cart controlller

---
+++ StoreExample/Controllers/CartController.cs
@@ -0,0 +1,25 @@
+﻿using StoreExample.DataBaseManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreExample.Controllers
+{
+    //контроллер для корзины
+    public class CartController : Controller
+    {
+        //установка сессии для корзины и получение доступа к ней
+        private CartManager GetCart()
+        {
+            CartManager cart = (CartManager)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new CartManager();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+}
