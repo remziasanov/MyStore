790b04a68d45ffc8d34d7bceba79f3a17140ec1f
remziasanov <35073857+remziasanov@users.noreply.github.com>
2018-04-05 12:38:34 +0300

    Edited Cart

--- StoreExample/Controllers/CartController.cs
+++ StoreExample/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 ﻿using StoreExample.DataBaseManager;
+using StoreExample.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,32 @@ namespace StoreExample.Controllers
                 Session["Cart"] = cart;
             }
             return cart;
+        }
+        public ActionResult Index()
+        {
+            var resultList = GetCart().Lines;
+            ViewBag.TotalSum = GetCart().ComputeTotalValue();
+            return View(resultList);
+        }
+        public RedirectToRouteResult AddToCart(Guid id)
+        {
+            PhoneItem phone = PhoneItemManager.Get(id);
+            if (phone != null)
+            {
+                GetCart().AddItem(phone, 1);
+            }
+            ViewBag.Count = GetCart().Lines.Count();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public RedirectToRouteResult RemoveFromCart(Guid id)
+        {
+            PhoneItem phone = PhoneItemManager.Get(id);
+            if (phone != null)
+            {
+                GetCart().RemoveLine(phone);
+            }
+            return RedirectToAction("Index", "Home");
         }
     }
 }
